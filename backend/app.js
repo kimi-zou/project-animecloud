45d278ac7af10f901bba14849f8f66bd39100eeb
Kimi Zou <kimizou.kz@gmail.com>
2020-12-17 16:55:29 -0500

    Add server error handlers

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
+
 
 // Internal dependencies
 const { environment } = require('./config');
@@ -32,5 +34,35 @@ app.use(csurf({ cookie: {
 
 // Express Middleware
 app.use(routes);
+
+// Handle resource-not-found error 
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.")
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err); // Pass err to the next error handler
+});
+
+// Handle sequelize errors
+app.use((err, _req, _res, next) => {
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map(e => e.message);
+    err.title = "Validation error";
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
 module.exports = app;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+// API test route
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
