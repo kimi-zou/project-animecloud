a4c21a021f281c718d88d117ed5e457f40867c44
Kimi Zou <kimizou.kz@gmail.com>
2020-12-17 18:20:15 -0500

    Add methods to User Model for authentication

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 // External dependencies
+const bcrypt = require('bcryptjs');
 const { Validator } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
@@ -49,5 +50,51 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function(models) {
     // associations can be defined here
   };
+
+  //--------------   Methods ---------------
+  // 1. Return user object that's safe to save to a JWT
+  User.prototype.toSafeObject = function () { // No arrow function
+    const { id, username, email } = this;
+    return { id, username, email };
+  };
+
+  // 2. Verify password
+  User.prototype.validatePassword = function(password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+  };
+
+  //--------------  Static Methods (not work for instances) ---------------
+  // 1. get user by id
+  User.getCurrentUserById = async function (id) {
+    return await User.scope('currentUser').findByPk(id);
+  }
+
+  // 2. login
+  User.login = async function ({ credential, password}) {
+    const { Op } = require('sequelize');
+    const user = await User.scope('loginUser').findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential,
+        },
+      },
+    });
+    if (user && user.validatePassword(password)) {
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+  };
+
+  // 3. signup
+  User.signup = async function ({ username, email, password }) {
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+      username,
+      email,
+      hashedPassword,
+    });
+    return await User.scope('currentUser').findByPk(user.id);
+  };
+
   return User;
 };
