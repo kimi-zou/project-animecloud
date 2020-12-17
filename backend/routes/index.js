be943ba05937faa69b316c25f751938a8efb53ed
Kimi Zou <kimizou.kz@gmail.com>
2020-12-17 16:36:06 -0500

    Set up api test route

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,14 @@
+// External dependencies
 const express = require('express');
 const router = express.Router();
 
+// Internal dependencies
+const apiRouter = require('./api');
+
+// Moute routes
+router.use('/api', apiRouter);
+
+// Test route
 router.get('/test', (req, res) => {
   res.cookie('XSRF-TOKEN', req.csrfToken());
   res.send('hello world!');
