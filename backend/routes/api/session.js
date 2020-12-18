9f28d55fea2769c800925a55037502f23ef54997
Kimi Zou <kimizou.kz@gmail.com>
2020-12-17 19:25:05 -0500

    Set up API Log out route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -25,4 +25,10 @@ router.post("/", asyncHandler(async(req, res, next) => {
   return res.json({ user });
 }));
 
+// Log out
+router.delete("/", (_req, res) => {
+  res.clearCookie("token");
+  return res.json({ message: "success" });
+});
+
 module.exports = router;
