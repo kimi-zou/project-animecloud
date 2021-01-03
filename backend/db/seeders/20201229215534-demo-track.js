308e26b490232c1b0441a6567eb3029891948fdd
Kimi Zou <kimizou.kz@gmail.com>
2021-01-03 18:20:29 -0500

    Update Track Seeder

--- backend/db/seeders/20201229215534-demo-track.js
+++ backend/db/seeders/20201229215534-demo-track.js
@@ -1,4 +1,5 @@
 'use strict';
+const faker = require('faker');
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -9,6 +10,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/1-c.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/1-The+View+From+the+Summit.m4a",
           userId: 2,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "Grief And Sorrow",
@@ -16,6 +19,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/3-c1.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/3-Grief+And+Sorrow.m4a",
           userId: 4,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "Main Theme",
@@ -23,6 +28,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/3-c2.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/3-Naruto+Main+Theme.m4a",
           userId: 4,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "Sakuras Theme",
@@ -30,6 +37,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/3-c3.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/3-Sakuras+Theme.m4a",
           userId: 4,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "The Raising Fighting Spirit",
@@ -37,6 +46,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/3-c4.jpeg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/3-The+Raising+Fighting+Spirit.m4a",
           userId: 4,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "Ultimate Secrets",
@@ -44,6 +55,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/3-c5.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/3-Ultimate+Secrets+.m4a",
           userId: 4,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "A Haunted House",
@@ -51,6 +64,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/5-c1.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/5-A+Haunted+House.m4a",
           userId: 6,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "Mei is Missing",
@@ -58,6 +73,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/5-c2.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/5-Mei+is+Missing.m4a",
           userId: 6,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "Path of the Wind",
@@ -65,6 +82,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/5-c3.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/5-Path+of+the+Wind.m4a",
           userId: 6,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "Tonari no Totoro",
@@ -72,6 +91,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/5-c4.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/5-Tonari+no+Totoro.m4a",
           userId: 6,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "A Reliable Friend!",
@@ -79,6 +100,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/8-c1.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/8-A+Reliable+Friend!.m4a",
           userId: 9,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "Gold And Oden",
@@ -86,6 +109,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/8-c2.jpeg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/8-Gold+And+Oden.m4a",
           userId: 9,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "Shizuka Na Ikari",
@@ -93,6 +118,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/8-c3.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/8-Shizuka+Na+Ikari.m4a",
           userId: 9,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "Let the stars fall down",
@@ -100,6 +127,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/9-c1.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/9-Let+the+stars+fall+down.m4a",
           userId: 10,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
         {
           title: "Point Zero",
@@ -107,6 +136,8 @@ module.exports = {
           coverImg: "https://animecloud.s3.amazonaws.com/mock-data/mock-covers/9-c2.jpg",
           trackPath: "https://animecloud.s3.amazonaws.com/mock-data/mock-tracks/9-Point+Zero.m4a",
           userId: 10,
+          createdAt: faker.date.past(1, new Date(2020, 8, 0)),
+          updatedAt: faker.date.recent(60, new Date())
         },
     ], {});
   },
